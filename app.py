30f750c31c7d28e0354bd3d51f00b1ebd55a692a
jsk0301 <john.sj.kim@yale.edu>
2019-01-26 22:06:05 -0500

    finished file uploading

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
 from werkzeug.utils import secure_filename
-from tempfile import mkdtemp
 from cs50 import SQL
 
 from genome_analysis import analyze
@@ -11,12 +10,22 @@ from genome_analysis import analyze
 UPLOAD_FOLDER = "./vcfs"
 ALLOWED_EXTENSIONS = set(["vcf"])
 
+# Configure application
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
@@ -53,6 +62,10 @@ def upload():
 
 		location = int(request.form.get("location"))
 
+		print(id)
+		print(location)
+		print(request.files)
+
 		if 'file' not in request.files:
 			flash('No file part')
 			return redirect(request.url)
@@ -78,6 +91,6 @@ def upload():
 			# 	 db.execute("UPDATE 'recipient_genotypes' SET :column = :gene WHERE id = :id", column=i, gene=gene, id=id)
 			# 	 i = i + 1
 
-			# os.remove(file_path)
+			os.remove(filepath)
 			flash('Upload Successful')
 			return redirect(request.url)
