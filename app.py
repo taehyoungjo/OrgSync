2939c6c7b33b8d9fe1e5065c68e359f7ca680153
jsk0301 <john.sj.kim@yale.edu>
2019-01-26 20:04:27 -0500

    now a runnable version!

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
+from flask_session import Session
 from werkzeug.utils import secure_filename
+from tempfile import mkdtemp
+from cs50 import SQL
 
 from genome_analysis import analyze
 
@@ -35,41 +38,42 @@ def search():
         return render_template("search.html", id=id)
 
     elif request.method == "POST":
+    	return render_template("search.html")
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
 	if request.method == "GET":
 		return render_template("upload.html")
 
-	elif request.method == "POST"
+	elif request.method == "POST":
 
 		id = int(request.form.get("id"))
 
 		location = int(request.form.get("location"))
 
-    	if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-    	file = request.files["file"]
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files["file"]
 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-        	filename = secure_filename(file.filename)
-        	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-   	    	filepath = "./vcfs/" + filename
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			filepath = "./vcfs/" + filename
 
-	    	genotype = analyze(file_path)
+			genotype = analyze(file_path)
 
-    		db.execute("INSERT INTO 'recipients' ('id','location') VALUES (:id, :location)", id=id, location=location)
-    		db.execute("INSERT INTO 'recipient_genotypes' ('id') VALUES (:id)", id=id)
-    		i = 1
-    		
-    		for gene in genotype:
-    			db.execute("UPDATE 'recipient_genotypes' SET :column = :gene WHERE id = :id", column=i, gene=gene id=id)
-    			i++
+			db.execute("INSERT INTO 'recipients' ('id','location') VALUES (:id, :location)", id=id, location=location)
+			db.execute("INSERT INTO 'recipient_genotypes' ('id') VALUES (:id)", id=id)
+			i = 1
 
-	    	os.remove(file_path)
-	    	return render_template("index.html")
+			for gene in genotype:
+				db.execute("UPDATE 'recipient_genotypes' SET :column = :gene WHERE id = :id", column=i, gene=gene, id=id)
+				i = i + 1
+
+			os.remove(file_path)
+			return render_template("index.html")
